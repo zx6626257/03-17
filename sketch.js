95fa602742aa70a293b17e56f55a7227edd81a52
zx6626257 <80819373+zx6626257@users.noreply.github.com>
2021-03-17 17:44:22 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = https://zx6626257.github.io/03-17/index.html/model.json';
 
 
 function preload() {
